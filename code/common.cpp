9d39216c4db44be672f6bd076a164d023021b84d
fpoisson2 <francis.poisson2@gmail.com>
2025-05-06 11:25:57 -0400

    Update common.cpp

--- code/common.cpp
+++ code/common.cpp
@@ -34,8 +34,8 @@ char scratchBuf[96];
 SoftwareSerial moduleSerial(swRxPin, swTxPin);
 
 const unsigned long moduleBaudRate = 9600UL;
-const char* APN = "onomondo";
-//const char* APN = "em";
+//const char* APN = "onomondo";
+const char* APN = "em";
 
 const char* TRACCAR_HOST = "trackteur.ve2fpd.com";
 const uint16_t TRACCAR_PORT = 5055;
