30343be07c2d761d1f851bd62f8ec63ffbaf8733
Francis Poisson <francis.poisson2@gmail.com>
2025-12-08 10:00:46 -0500

    cleanup

--- code/traccar.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACCAR_H
-#define TRACCAR_H
-
-#include <Arduino.h>
-
-bool sendGpsToTraccar(const char* host, uint16_t port, const char* deviceId,
-                      float lat, float lon, const char* timestampStr);
-
-#endif
